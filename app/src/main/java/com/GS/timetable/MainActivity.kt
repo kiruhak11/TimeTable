8e01733a73ee3b9610b388eb99d7a5f8028cfde2
kiruhak11 <kiruhak2005@gmail.com>
2022-03-11 15:17:02 +0300

    первый раз

---
+++ app/src/main/java/com/GS/timetable/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.GS.timetable
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val bottomNavigationView = findViewById<BottomNavigationView
+                >(R.id.bottom_navigatin_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(
+            navController
+        )
+
+    }
+}
